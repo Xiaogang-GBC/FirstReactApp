2578a110345e8c4b3eceff2c3fec0756c4c456d8
Xiaogang Dong <xiaogangdong@xiaogangs-air.home>
2023-10-04 15:06:01 -0400

    initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import React from 'react'
+import Greeter from './components/Greeter';
+
+function App() {
+  return (
+    <div>
+      <h1>Hello React!</h1>
+      <Greeter />
+    </div>
+  )
+}
+
+export default App;
---
+++ src/components/Greeter.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Greeter(){
+    return <h2>This is my greeting from Greeter component!</h2>
+}
+
+export default Greeter;
